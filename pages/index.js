da3190f58b7b38c33c5e3406aafc0b6bdacd57e3
Josue Mamani <38815723+meeighteen@users.noreply.github.com>
2022-03-16 23:47:01 -0500

    fix: update description

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,55 @@ export default function Home() {
         <Header title="Tumi Delivery App" />
         <p className="description">
           Documentacion de Politica de Privacidad
+    Política de Privacidad de Tumi Delivery y Tratamiento de Datos Personales
+Política de Privacidad de Tumi Delivery y Tratamiento de Datos Personales
+De conformidad con lo establecido en la Ley N° 29733, Ley de Protección de Datos Personales y el Decreto Supremo N° 003-2013-JUS, Reglamento de la Ley de Protección de Datos Personales, TUMI DELIVERY S.A.C., con Registro Único de Contribuyentes (“RUC”) N° XXXXXXXXXXX, con domicilio en Av. XXXXXXXXXXXXXXXXX, Lima, Perú (en adelante, “TUMI DELIVERY”), le informa mediante esta Política de Privacidad (en adelante, la “Política”), las condiciones del tratamiento de su información.
+El Titular consiente de manera libre, expresa, previa e informada que TUMI DELIVERY trate los datos personales que usted nos proporcione mediante los formularios electrónicos que se le proponga y los almacene en un banco de datos de su titularidad para los fines establecidos en esta Política.
+Se informa que la información del Titular se almacenará en uno de los siguientes bancos de datos personales, por el plazo señalado, según corresponda:
+1) Si usted se registra como Usuario Consumidor en nuestras Aplicaciones y Sitio Web
+Nombre de banco de datos personales: Banco de datos de usuarios consumidores
+Plazo de almacenamiento: Durante el plazo que dure la relación contractual entre TUMI DELIVERY y usted más 10 años.
+2) Si usted se registra como Usuario Tummer en nuestras Aplicaciones y Sitio Web
+Nombre de banco de datos personales: Banco de datos de repartidores independientes
+Plazo de almacenamiento: Durante el plazo que dure la relación contractual entre TUMI DELIVERY y usted más 10 años.
+PRIMERA: Derechos de acceso, cancelación y rectificación de la Información Personal
+1.1. El Titular puede ejercer los derechos de acceso, rectificación, cancelación u oposición, establecidos en la normativa de protección de datos personales.
+De considerar el Titular que no ha sido atendido en el ejercicio de sus derechos, puede presentar un reclamo ante la Dirección de Protección de Datos Personales del Ministerio de Justicia y Derechos Humanos.
+1.2 En caso que el Titular se registre como Usuario consumidor o Usuario Tummer, podrá revisar y cambiar la información que ha enviado a TUMI DELIVERY, por motivos debidamente justificados y legítimos, incluyendo:
+el usuario y dirección de correo electrónico.
+la información de registro como: domicilio, ciudad, región, código postal, número principal de teléfono, número secundario de teléfono, correo electrónico, etc.
+la clave.
+1.3. TUMI DELIVERY podrá modificar esta Política. En dicho caso, TUMI DELIVERY notificará al Titular publicando una versión actualizada de la Política en esta página y mediante el envío de un e-mail o informándolo en el Sitio Web o Aplicaciones, a efectos de mantener actualizado al Titular. El Titular acepta que, si después de dicha notificación, mantiene su registro en el Sitio Web y Aplicaciones, es porque está de acuerdo con las mismas. Si no está de acuerdo, el Titular deberá solicitar darse de baja a TUMI DELIVERY, en cuyo caso, TUMI DELIVERY no tratará más su información, sino únicamente para el cumplimiento de la ley aplicable.
+SEGUNDA: La Información Personal que recopilamos
+2.1. A fin de acceder como Usuario Consumidor a la aplicación Tumi Delivery, los Titulares deben registrarse, suministrando ciertos datos personales completos y exactos. TUMI DELIVERY podrá solicitar, recabar y almacenar la siguiente información personal: apodo o seudónimo (nombre de Titular de los datos) para operar en el sitio de TUMI DELIVERY, nombre y apellido, número de documento o identificación válida, información física de contacto (como número de teléfono, domicilio, tipo de domicilio, dirección de e-mail, etc.)
+En adelante, la “Información Personal”.
+TERCERA: Finalidad y Uso de la Información Personal
+3.1. Su información será tratada de acuerdo a las siguientes finalidades y usos, según corresponda:
+Si usted es un Usuario Tummer
+La Información Personal será tratada para ejecutar la relación contractual entre TUMI DELIVERY y el Tummer y para brindar los servicios a los Usuarios Consumidores a través de la Aplicacion, de acuerdo a sus términos y condiciones.
+TUMI DELIVERY podrá compartir los datos de los Tummers a los Usuarios para que estos se comuniquen ante cualquier eventualidad que surja durante la prestación de los servicios de reparto.
+Si usted es Tummer, para que pueda aceptar y tramitar las órdenes más cercanas a su ubicación, la Aplicación “Tumi Delivery” recogerá datos de su localización, aún cuando la Aplicación se encuentre cerrada o no esté en uso.
+El app ‘Tumi Delivery’ recoge y almacena el número telefónico del usuario el cual es almacenado en nuestra base de datos usando métodos de encriptación para garantizar la protección de la información personal del usuario. La data recolectada se utiliza para identificar posibles comportamientos atípicos dentro de la aplicación y no es compartida ni transferida a terceros.
+Si usted es un usuario Consumidor
+La Información Personal será tratada para ejecutar la relación contractual entre TUMI DELIVERY y el Usuario Consumidor a través de la Aplicacion, de acuerdo a sus términos y condiciones.
+La Información Personal, sólo podrá ser utilizada por los Tummers en el marco de la prestación del servicio de reparto y no deberá ser empleada para actividades no relacionadas con y autorizadas por TUMI DELIVERY. Consentimiento para fines promocionales
+
+CUARTA: Confidencialidad de la Información Personal
+4.1. TUMI DELIVERY no venderá, intercambiará, alquilará o compartirá la Información Personal del Titular excepto en las formas establecidas en esta Política.
+QUINTA: Clave Personal
+5.1. Para acceder a los servicios reservados, el Titular dispondrá de una clave personal. Con ella podrá contratar y calificar los servicios que prestan por terceros a través de la Aplicacion. El Titular deberá mantener esta clave bajo absoluta confidencialidad y, en ningún caso, deberá revelarla o compartirla con otras personas.
+5.2. El Titular será responsable de todos los actos que tengan lugar mediante el uso de su nombre de Titular y clave, lo que incluye asumir la responsabilidad por los daños que puedan sufrir otros Titulares mediante su cuenta. Si, por cualquier razón, un Titular creyera que alguien puede conocer su clave, deberá modificarla ingresando a la opción de modificación dispuesta para tal fin en la plataforma de TUMI DELIVERY.
+SEXTA: Menores de Edad
+6.1. Debes tener más de 14 años para registrarte en la APP o el Sitio como usuario consumidor. Para registrarte como usuario Tummer, deberás tener más de 18 años. Está, por tanto, restringido el registro en la App o el Sitio que no cumpla con los límites mínimos de edad.
+6.2 TUMI DELIVERY se reserva el derecho de verificar, por los medios legales que considere más oportunos y relevantes, la edad real de cualquier Titular de los datos.
+6.3. Ante una sospecha razonable de que un Titular de los datos de la Aplicación se ha registrado incumpliendo los límites mínimos de edad o que ha falseado los datos que se requieren para su acceso, TUMI DELIVERY podrá denegar el acceso a sus servicios.
+6.4.  En ningún caso, TUMI DELIVERY solicitará el consentimiento de menores para el tratamiento de datos personales para el acceso a actividades, vinculadas con bienes o servicios que están restringidos para mayores de edad.
+SÉPTIMA: Requerimientos Legales
+7.1. TUMI DELIVERY cooperará con las autoridades competentes y con otros terceros para garantizar el cumplimiento de las leyes. TUMI DELIVERY podrá revelar la Información Personal del Titular sin su consentimiento únicamente cuando esté permitido por ley, requerido por mandato judicial, o en casos de prevención de lavado de activos o financiamiento del terrorismo o el caso que dicho Titular participa de actividades ilegales. En tales situaciones, TUMI DELIVERY colaborará con las autoridades competentes con el fin de salvaguardar la integridad y la seguridad de la comunidad y la de sus Titulares de los datos.
+OCTAVA: Seguridad y Almacenamiento de la Información Personal
+8.1. En el tratamiento de la Información Personal, TUMI DELIVERY garantiza que ha adoptado medidas técnicas para garantizar la seguridad de sus datos personales. Sin perjuicio de lo expuesto, considerando que internet es un sistema abierto, de acceso público, TUMI DELIVERY no puede garantizar que terceros no autorizados no puedan eventualmente superar las medidas de seguridad y utilizar la Información Personal en forma indebida, por tratarse de eventos imprevisibles.
+8.2. El Titular conoce y acepta expresamente que TUMI DELIVERY, a su exclusivo criterio, recabe, almacene y eventualmente monitoree el intercambio de mensajes y correos electrónicos entre el Usuario Consumidor y Usuario Tummer, efectuado dentro de las Aplicaciones de TUMI DELIVERY, con el único objetivo de contribuir a la seguridad de las relaciones y comunicaciones que se desarrollan a través de éstas.
+
         </p>
       </main>
 
